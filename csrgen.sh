62049c7610432612cb36d8e56eca52f69e731768
GitGudAuth <117452333+GitGudAuth@users.noreply.github.com>
2024-10-26 12:15:50 +0700

    Update csrgen.sh

--- csrgen.sh
+++ csrgen.sh
@@ -1,26 +1,32 @@
 #!/bin/bash    
     echo -n "Enter domain name: "
     read domain
-    cd /home/*/SSL
+    DIR="$HOME/SSL"
+    if [ ! -d "$DIR" ]; then
+	    mkdir -pv "$DIR"
+	    echo "Directory $DIR created."
+	    ls "$DIR"
+	    echo ""
+    fi
+    
+    cd $HOME/SSL 
     country=VN
     state=HCM
     locality=HCM
     organization=$domain
     organizationunit=IT
     openssl req -newkey rsa:2048 -nodes -keyout "$domain".key -out "$domain".csr -subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizationunit/CN=$domain"
-    cd ~
-    ls -l /home/*/SSL
+
     echo "Generating csr and private key complete !"
     echo 
     echo "---------------------------"
     echo "-----Below is your CSR-----"
     echo "---------------------------"
     echo
-    cat /home/*/SSL/$domain.csr
+    cat $HOME/SSL/$domain.csr
     echo
     echo "---------------------------"
     echo "-----Below is your Key-----"
     echo "---------------------------"
     echo
-    cat /home/*/SSL/$domain.key
-
+    cat $HOME/SSL/$domain.key
